e4f831186f18eb3f0ab21b34c27bda8d12aff556
Waseem Akram <hackerbad669@gmail.com>
2023-11-08 15:19:01 +0500

    Refactored code to improve performance and readability.

---
+++ simple-calc.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    int a, b, sum;
+    cout << "Enter First Number : ";
+    cin >> a;
+    cout << "Enter Second Number : ";
+    cin >> b;
+    sum = a + b;
+    cout << "The Sum of " << a << " and " << b << " is : " << sum << endl;
+    return 0;
+}
