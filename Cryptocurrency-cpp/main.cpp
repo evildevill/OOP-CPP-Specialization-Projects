94510dd78421385e1d46619f60f9c00ec09b67b3
Waseem Akram <hackerbad669@gmail.com>
2023-11-10 19:21:23 +0500

    Add menu printing function and user input handling

--- Cryptocurrency-cpp/main.cpp
+++ Cryptocurrency-cpp/main.cpp
@@ -3,29 +3,34 @@
 #include <cstdlib>   // for system("cls") function
 using namespace std; // Using the standard namespace
 
+void PrintMenu()
+{
+    // Print the menu
+    cout << "==============================================" << endl;
+    cout << "====Welcome to the Cryptocurrency Exchaneg====" << endl;
+    cout << "==============================================" << endl;
+    // cout << endl; // Print a new line
+    cout << "1: Print Help" << endl;
+    cout << "2: Print Exchange Stats" << endl;
+    cout << "3: Make an offer" << endl;
+    cout << "4: Make a bid" << endl;
+    cout << "5: Print Wallet" << endl;
+    cout << "6: Continue" << endl;
+    cout << "7: Quit" << endl;
+}
+
 int main() // Main function
 {
-    while (true)
+    // Print the menu
+    PrintMenu();
+    int UserOption;                                 // Variable to store user input
+    cout << "============================" << endl; // Print the menu
+    cout << "Enter your choice (1 - 6): ";          // Get user input for the option number
+    cin >> UserOption;                              // Store the user input in the variable
+    cout << "You Entered: " << UserOption << endl;  // Print the user input
+    cout << "============================" << endl; // Print the menu
+    while (true)                                    // Check the user input and print the corresponding message
     {
-        int UserOption; // Variable to store user input
-        // Print the menu
-        cout << "==============================================" << endl;
-        cout << "====Welcome to the Cryptocurrency Exchaneg====" << endl;
-        cout << "==============================================" << endl;
-        // cout << endl; // Print a new line
-        cout << "1: Print Help" << endl;
-        cout << "2: Print Exchange Stats" << endl;
-        cout << "3: Make an offer" << endl;
-        cout << "4: Make a bid" << endl;
-        cout << "5: Print Wallet" << endl;
-        cout << "6: Continue" << endl;
-        cout << "7: Quit" << endl;
-        cout << "============================" << endl; // Print the menu
-        cout << "Enter your choice (1 - 6): ";          // Get user input for the option number
-        cin >> UserOption;                              // Store the user input in the variable
-        cout << "You Entered: " << UserOption << endl;  // Print the user input
-        cout << "============================" << endl; // Print the menu
-        // Check the user input and print the corresponding message
         if (UserOption == 0) // Bad Choice
         {
             cout << "Invalid Choice :(" << endl;
@@ -34,10 +39,12 @@ int main() // Main function
             sleep(2);
             // Clear the screen.
             system("cls");
+            // Print the menu 
+            PrintMenu();
         }
         else if (UserOption == 1) // Print Help
         {
-            
+
             cout << "Help - Your Aim is to make money. Analyse the market and makes bid" << endl;
         }
         else if (UserOption == 2) // Print Exchange Stats
