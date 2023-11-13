60ccb0682395f89751a11194de6282af0085be79
Waseem Akram <hackerbad669@gmail.com>
2023-11-13 12:59:51 +0500

    Refactored main.cpp to include necessary libraries and added functionality for order book

--- Cryptocurrency-cpp/main.cpp
+++ Cryptocurrency-cpp/main.cpp
@@ -1,10 +1,12 @@
-#include <iostream>
-#include <unistd.h>
-#include <cstdlib>
+#include <iostream> // USED FOR INPUT AND OUTPUT
+#include <string>   // USED FOR STRINGS
+#include <vector>   // USED FOR VECTORS
+#include <unistd.h> // USED FOR SLEEP
+#include <cstdlib>  // USED FOR SYSTEM(CLS)
 
-using namespace std;
+using namespace std; // USED FOR STD FUNCTIONS
 
-void PrintMenu()
+void PrintMenu() // PRINTS THE MENU
 {
     cout << "==============================================" << endl;
     cout << "====Welcome to the Cryptocurrency Exchange====" << endl;
@@ -18,53 +20,53 @@ void PrintMenu()
     cout << "7: Quit" << endl;
 }
 
-void PrintHelp()
+void PrintHelp() // PRINTS THE HELP
 {
     cout << "Help - Your Aim is to make money. Analyze the market and make a bid" << endl;
 }
 
-void PrintExchangeStats()
+void PrintExchangeStats() // PRINTS THE EXCHANGE STATS
 {
     cout << "Exchange Stats - Looks Good" << endl;
 }
 
-void MakeAnOffer()
+void MakeAnOffer() // PRINTS THE MAKE AN OFFER
 {
     cout << "Make an offer - Please Enter Your desired amount" << endl;
 }
 
-void MakeABid()
+void MakeABid() // PRINTS THE MAKE A BID
 {
     cout << "Make a bid - Please Enter Your desired amount" << endl;
 }
 
-void PrintWallet()
+void PrintWallet() // PRINTS THE WALLET
 {
     cout << "Wallet - Your Wallet is empty :(" << endl;
 }
 
-void Continue()
+void Continue() // PRINTS THE CONTINUE
 {
     cout << "Continue - Going to the next Time Frame" << endl;
 }
 
-void Quit()
+void Quit() // PRINTS THE QUIT
 {
     cout << "Quit - Bye Bye" << endl;
 }
 
-int getUserOption()
+int getUserOption() // GETS THE USER OPTION
 {
-    int UserOption;
+    int UserOption; // UserOption is an integer
     cout << "============================" << endl;
     cout << "Enter your choice (1 - 7): ";
-    cin >> UserOption;
+    cin >> UserOption; // UserOption is the input
     cout << "You Entered: " << UserOption << endl;
     cout << "============================" << endl;
-    return UserOption;
+    return UserOption; // Returns the UserOption
 }
 
-void ProcessUserOption(int UserOption)
+void ProcessUserOption(int UserOption) // PROCESSES THE USER OPTION
 {
     if (UserOption == 0) // Bad Choice
     {
@@ -108,14 +110,49 @@ void ProcessUserOption(int UserOption)
     }
 }
 
-int main()
+int main() // MAIN FUNCTION
 {
-    while (true)
+    enum class OrderBookType
     {
-        PrintMenu();
-        int UserOption = getUserOption();
-        ProcessUserOption(UserOption);
-    }
+        BID,
+        ASK,
+        SHOUT
+    }; // ENUM CLASS FOR ORDER BOOK TYPE
 
-    return 0;
+    // double Price = 5319.450228; // DOUBLE FOR PRICE
+    // double Amount = 0.00020075; // DOUBLE FOR AMOUNT
+    // string TimeStamp{"2023/11/13 17:01:24.884492"}; // STRING FOR TIMESTAMP
+    // string Product{"BTC/USD"}; // STRING FOR PRODUCT
+    // // string OderType{"anything"};
+    // OrderBookType OrderType = OrderBookType::ASK; // ORDER TYPE IS ASK
+
+    vector<double> Prices;            // VECTOR FOR PRICES
+    vector<double> Amounts;           // VECTOR FOR AMOUNTS
+    vector<string> TimeStamps;        // VECTOR FOR TIMESTAMPS
+    vector<string> Products;          // VECTOR FOR PRODUCTS
+    vector<OrderBookType> OrderTypes; // VECTOR FOR ORDER TYPES
+
+    Prices.push_back(5000.01);                          // PUSH BACK THE PRICE
+    Amounts.push_back(0.001);                           // PUSH BACK THE AMOUNT
+    TimeStamps.push_back("2023/11/13 17:01:24.884492"); // PUSH BACK THE TIMESTAMP
+    Products.push_back("BTC/USD");                      // PUSH BACK THE PRODUCT
+    OrderTypes.push_back(OrderBookType::ASK);           // PUSH BACK THE ORDER TYPE
+
+    Prices.push_back(10.01);                            // PUSH BACK THE PRICE
+    Amounts.push_back(0.001);                           // PUSH BACK THE AMOUNT
+    TimeStamps.push_back("2023/11/13 17:01:24.884492"); // PUSH BACK THE TIMESTAMP
+    Products.push_back("BTC/USD");                      // PUSH BACK THE PRODUCT
+    OrderTypes.push_back(OrderBookType::BID);           // PUSH BACK THE ORDER TYPE
+
+    cout << "Price: " << Prices[0] << endl; // PRINTS THE PRICE
+    cout << "Price: " << Prices[1] << endl; // PRINTS THE PRICE
+
+    // while (true) // WHILE LOOP FOR THE MENU
+    // {
+    //     PrintMenu(); // Prints the menu
+    //     int UserOption = getUserOption(); // UserOption is the input
+    //     ProcessUserOption(UserOption); // Processes the UserOption
+    // }
+
+    return 0; // Returns 0
 }
