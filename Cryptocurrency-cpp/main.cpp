5caf38f425407c62cbcbb9fa3e165b4cd006e46f
Waseem Akram <hackerbad669@gmail.com>
2023-11-13 19:07:17 +0500

    Commented out unused code.

--- Cryptocurrency-cpp/main.cpp
+++ Cryptocurrency-cpp/main.cpp
@@ -149,23 +149,23 @@ int main()
     // // string OderType{"anything"};
     // OrderBookType OrderType = OrderBookType::ASK; // ORDER TYPE IS ASK
 
-    vector<double> Prices;            // VECTOR FOR PRICES
-    vector<double> Amounts;           // VECTOR FOR AMOUNTS
-    vector<string> TimeStamps;        // VECTOR FOR TIMESTAMPS
-    vector<string> Products;          // VECTOR FOR PRODUCTS
-    vector<OrderBookType> OrderTypes; // VECTOR FOR ORDER TYPES
+    // vector<double> Prices;            // VECTOR FOR PRICES
+    // vector<double> Amounts;           // VECTOR FOR AMOUNTS
+    // vector<string> TimeStamps;        // VECTOR FOR TIMESTAMPS
+    // vector<string> Products;          // VECTOR FOR PRODUCTS
+    // vector<OrderBookType> OrderTypes; // VECTOR FOR ORDER TYPES
 
-    Prices.push_back(5000.01);                          // PUSH BACK THE PRICE
-    Amounts.push_back(0.001);                           // PUSH BACK THE AMOUNT
-    TimeStamps.push_back("2023/11/13 17:01:24.884492"); // PUSH BACK THE TIMESTAMP
-    Products.push_back("BTC/USD");                      // PUSH BACK THE PRODUCT
-    OrderTypes.push_back(OrderBookType::ASK);           // PUSH BACK THE ORDER TYPE
+    // Prices.push_back(5000.01);                          // PUSH BACK THE PRICE
+    // Amounts.push_back(0.001);                           // PUSH BACK THE AMOUNT
+    // TimeStamps.push_back("2023/11/13 17:01:24.884492"); // PUSH BACK THE TIMESTAMP
+    // Products.push_back("BTC/USD");                      // PUSH BACK THE PRODUCT
+    // OrderTypes.push_back(OrderBookType::ASK);           // PUSH BACK THE ORDER TYPE
 
-    Prices.push_back(10.01);                            // PUSH BACK THE PRICE
-    Amounts.push_back(0.001);                           // PUSH BACK THE AMOUNT
-    TimeStamps.push_back("2023/11/13 17:01:24.884492"); // PUSH BACK THE TIMESTAMP
-    Products.push_back("BTC/USD");                      // PUSH BACK THE PRODUCT
-    OrderTypes.push_back(OrderBookType::BID);           // PUSH BACK THE ORDER TYPE
+    // Prices.push_back(10.01);                            // PUSH BACK THE PRICE
+    // Amounts.push_back(0.001);                           // PUSH BACK THE AMOUNT
+    // TimeStamps.push_back("2023/11/13 17:01:24.884492"); // PUSH BACK THE TIMESTAMP
+    // Products.push_back("BTC/USD");                      // PUSH BACK THE PRODUCT
+    // OrderTypes.push_back(OrderBookType::BID);           // PUSH BACK THE ORDER TYPE
 
     // cout << "Price: " << Prices[0] << endl; // PRINTS THE PRICE
     // cout << "Price: " << Prices[1] << endl; // PRINTS THE PRICE
