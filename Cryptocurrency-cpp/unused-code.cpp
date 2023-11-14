3862e98f706392473d285150428d5a370db13d68
Waseem Akram <hackerbad669@gmail.com>
2023-11-14 12:03:23 +0500

    Remove commented out code and unused variables in main.cpp

---
+++ Cryptocurrency-cpp/unused-code.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+int main (){
+    // double Price = 5319.450228; // DOUBLE FOR PRICE
+    // double Amount = 0.00020075; // DOUBLE FOR AMOUNT
+    // string TimeStamp{"2023/11/13 17:01:24.884492"}; // STRING FOR TIMESTAMP
+    // string Product{"BTC/USD"}; // STRING FOR PRODUCT
+    // // string OderType{"anything"};
+    // OrderBookType OrderType = OrderBookType::ASK; // ORDER TYPE IS ASK
+
+    // vector<double> Prices;            // VECTOR FOR PRICES
+    // vector<double> Amounts;           // VECTOR FOR AMOUNTS
+    // vector<string> TimeStamps;        // VECTOR FOR TIMESTAMPS
+    // vector<string> Products;          // VECTOR FOR PRODUCTS
+    // vector<OrderBookType> OrderTypes; // VECTOR FOR ORDER TYPES
+
+    // Prices.push_back(5000.01);                          // PUSH BACK THE PRICE
+    // Amounts.push_back(0.001);                           // PUSH BACK THE AMOUNT
+    // TimeStamps.push_back("2023/11/13 17:01:24.884492"); // PUSH BACK THE TIMESTAMP
+    // Products.push_back("BTC/USD");                      // PUSH BACK THE PRODUCT
+    // OrderTypes.push_back(OrderBookType::ASK);           // PUSH BACK THE ORDER TYPE
+
+    // Prices.push_back(10.01);                            // PUSH BACK THE PRICE
+    // Amounts.push_back(0.001);                           // PUSH BACK THE AMOUNT
+    // TimeStamps.push_back("2023/11/13 17:01:24.884492"); // PUSH BACK THE TIMESTAMP
+    // Products.push_back("BTC/USD");                      // PUSH BACK THE PRODUCT
+    // OrderTypes.push_back(OrderBookType::BID);           // PUSH BACK THE ORDER TYPE
+
+    // cout << "Price: " << Prices[0] << endl; // PRINTS THE PRICE
+    // cout << "Price: " << Prices[1] << endl; // PRINTS THE PRICE
+
+    // while (true) // WHILE LOOP FOR THE MENU
+    // {
+    //     PrintMenu(); // Prints the menu
+    //     int UserOption = getUserOption(); // UserOption is the input
+    //     ProcessUserOption(UserOption); // Processes the UserOption
+    // }
+
+    // // OderBookEntry order1;
+
+    // OderBookEntry order2{2000,
+    //                      0.003,
+    //                      "2023/11/13 17:01:24.884492",
+    //                      "BTC/USD",
+    //                      OrderBookType::BID};
+
+        // Orders.push_back(order2);
+
+    // order1.Price = 10000;
+    // order1.Amount = 0.002;
+    // order1.TimeStamp = "2023/11/13 17:01:24.884492";
+    // order1.Product = "BTC/USD";
+    // order1.OrderType = OrderBookType::BID;
+
+}
