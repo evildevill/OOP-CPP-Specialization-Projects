bf22663a3033b3301e0189d87392f6fd7cdd14e2
Waseem Akram <hackerbad669@gmail.com>
2024-04-05 10:52:58 +0500

    Delete unnecessary files and code

--- unused-code.cpp
+++ unused-code.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
-int main (){
+int main()
+{
     // double Price = 5319.450228; // DOUBLE FOR PRICE
     // double Amount = 0.00020075; // DOUBLE FOR AMOUNT
     // string TimeStamp{"2023/11/13 17:01:24.884492"}; // STRING FOR TIMESTAMP
@@ -44,7 +45,7 @@ int main (){
     //                      "BTC/USD",
     //                      OrderBookType::BID};
 
-        // Orders.push_back(order2);
+    // Orders.push_back(order2);
 
     // order1.Price = 10000;
     // order1.Amount = 0.002;
@@ -54,4 +55,178 @@ int main (){
 
     // cout << "Price: " << Orders[1].Price << endl;
     // return 0;
+
+    // main.cpp
+    //     #include <iostream>
+    // #include <string>
+    // #include <vector>
+
+    // using namespace std;
+
+    // enum class OrderBookType
+    // {
+    //     BID,
+    //     ASK,
+    //     SHOUT
+    // };
+
+    // class OderBookEntry
+    // {
+    // public:
+    //     OderBookEntry(double _Price,
+    //                   double _Amount,
+    //                   string _TimeStamp,
+    //                   string _Product,
+    //                   OrderBookType _OrderType)
+    //         : Price(_Price),
+    //           Amount(_Amount),
+    //           TimeStamp(_TimeStamp),
+    //           Product(_Product),
+    //           OrderType(_OrderType)
+    //     {
+    //     }
+
+    //     double Price;
+    //     double Amount;
+    //     string TimeStamp;
+    //     string Product;
+    //     OrderBookType OrderType;
+    // };
+
+    // void PrintMenu()
+    // {
+    //     cout << "==============================================" << endl;
+    //     cout << "====Welcome to the Cryptocurrency Exchange====" << endl;
+    //     cout << "==============================================" << endl;
+    //     cout << "1: Print Help" << endl;
+    //     cout << "2: Print Exchange Stats" << endl;
+    //     cout << "3: Make an offer" << endl;
+    //     cout << "4: Make a bid" << endl;
+    //     cout << "5: Print Wallet" << endl;
+    //     cout << "6: Continue" << endl;
+    //     cout << "7: Quit" << endl;
+    // }
+
+    // void PrintHelp()
+    // {
+    //     cout << "Help - Your Aim is to make money. Analyze the market and make a bid" << endl;
+    // }
+
+    // void PrintExchangeStats()
+    // {
+    //     cout << "Exchange Stats - Looks Good" << endl;
+    // }
+
+    // void MakeAnOffer()
+    // {
+    //     cout << "Make an offer - Please Enter Your desired amount" << endl;
+    // }
+
+    // void MakeABid()
+    // {
+    //     cout << "Make a bid - Please Enter Your desired amount" << endl;
+    // }
+
+    // void PrintWallet()
+    // {
+    //     cout << "Wallet - Your Wallet is empty :(" << endl;
+    // }
+
+    // void Continue()
+    // {
+    //     cout << "Continue - Going to the next Time Frame" << endl;
+    // }
+
+    // void Quit()
+    // {
+    //     cout << "Quit - Bye Bye" << endl;
+    // }
+
+    // int getUserOption()
+    // {
+    //     int UserOption;
+    //     cout << "============================" << endl;
+    //     cout << "Enter your choice (1 - 7): ";
+    //     cin >> UserOption;
+    //     cout << "You Entered: " << UserOption << endl;
+    //     cout << "============================" << endl;
+    //     return UserOption;
+    // }
+
+    // void ProcessUserOption(int UserOption)
+    // {
+    //     if (UserOption == 0)
+    //     {
+    //         cout << "Invalid Choice :(" << endl;
+    //         cout << "Please enter a number between 1 to 7" << endl;
+    //     }
+    //     else if (UserOption == 1)
+    //     {
+    //         PrintHelp();
+    //     }
+    //     else if (UserOption == 2)
+    //     {
+    //         PrintExchangeStats();
+    //     }
+    //     else if (UserOption == 3)
+    //     {
+    //         MakeAnOffer();
+    //     }
+    //     else if (UserOption == 4)
+    //     {
+    //         MakeABid();
+    //     }
+    //     else if (UserOption == 5)
+    //     {
+    //         PrintWallet();
+    //     }
+    //     else if (UserOption == 6)
+    //     {
+    //         Continue();
+    //     }
+    //     else if (UserOption == 7)
+    //     {
+    //         Quit();
+    //     }
+    //     else
+    //     {
+    //         cout << "Invalid Choice :(" << endl;
+    //         cout << "Please enter a number between 1 to 7" << endl;
+    //     }
+    // }
+
+    // int main()
+    // {
+    //     vector<OderBookEntry> Orders;
+    //     Orders.push_back(OderBookEntry{1000,
+    //                                    0.002,
+    //                                    "2023/11/13 17:01:24.884492",
+    //                                    "BTC/USD",
+    //                                    OrderBookType::BID});
+
+    //     Orders.push_back(OderBookEntry{2000,
+    //                                    0.002,
+    //                                    "2023/11/13 17:01:24.884492",
+    //                                    "BTC/USD",
+    //                                    OrderBookType::BID});
+
+    //     // Three ways to iterate over a vector
+
+    //     for (OderBookEntry &Order : Orders)
+    //     {
+    //         cout << "The Price is : " << Order.Price << endl;
+    //     }
+
+    //     for (unsigned int i = 0; i < Orders.size(); ++i)
+    //     {
+    //         cout << "The Price is : " << Orders[i].Price << endl;
+    //     }
+
+    //     for (unsigned int i = 0; i < Orders.size(); ++i)
+    //     {
+    //         cout << "The Price is : " << Orders.at(i).Price << endl;
+    //     }
+
+    //     // End of three ways to iterate over a vector
+    // }
 }
